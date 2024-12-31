5ffba3f3b9d4be7187bf5b607ef5844ab9dfad05
Dhanish kumar <xpstm1@gmail.com>
2024-12-31 21:20:16 +0530

    Darkmode

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { School } from 'lucide-react'
 import React from 'react'
 
 const Navbar = ()=> {
+  const user =true;
   return (
     <div className="h-16 dark:bg-[#020817] bg-white border-b dark:border-b-gray-800 border-b-gray-200 fixed top-0 left-0 right-0 duration-300 z-10">
      
@@ -10,8 +11,10 @@ const Navbar = ()=> {
       <School size={"30"}/>
       <h1 className="hidden md:block font-extrabold text-2xl">
        E-Learning
-            </h1>
+      </h1>
       </div>
+
+
     </div>
   );
 };
